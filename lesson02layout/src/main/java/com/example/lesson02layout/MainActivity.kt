d1979835a7de598e3be7480af0eea9c312649bab
vburmaka <vburmaka@griddynamics.com>
2022-10-14 17:33:26 +0300

    first commit

--- lesson02layout/src/main/java/com/example/lesson02layout/MainActivity.kt
+++ lesson02layout/src/main/java/com/example/lesson02layout/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.lesson02layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val titleTextView = findViewById<TextView>(R.id.mainActivityTitleTextView)
+        val button = findViewById<Button>(R.id.mainActivityButton)
+
+        button.setOnClickListener(object : OnClickListener{
+            override fun onClick(p0: View?) {
+                titleTextView.text = button.text
+            }
+        })
     }
 }
