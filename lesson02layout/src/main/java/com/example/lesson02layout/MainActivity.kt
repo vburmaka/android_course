efd80cc3f541b74731aeb371660407b0a89745cd
vburmaka <vburmaka@griddynamics.com>
2022-10-21 18:13:32 +0300

    layout practice final. TODO layout + interactions

--- lesson02layout/src/main/java/com/example/lesson02layout/MainActivity.kt
+++ lesson02layout/src/main/java/com/example/lesson02layout/MainActivity.kt
@@ -5,12 +5,40 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private var mainEditText: EditText? = null
+    private var sevenButton: Button? = null
+    private var eightButton: Button? = null
+    private var nineButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainEditText = findViewById(R.id.mainEditText)
+
+        sevenButton = findViewById(R.id.sevenButton)
+        eightButton = findViewById(R.id.eightButton)
+        nineButton = findViewById(R.id.nineButton)
+
+        sevenButton?.setOnClickListener(this)
+        eightButton?.setOnClickListener(this)
+        nineButton?.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        mainEditText?.let {
+            when (view.id) {
+                R.id.sevenButton -> mainEditText?.setText(it.text.toString() + "7")
+                R.id.eightButton -> mainEditText?.setText(it.text.toString() + "8")
+                R.id.nineButton -> mainEditText?.setText(it.text.toString() + "9")
+                else ->{}
+            }
+        }
     }
 }
